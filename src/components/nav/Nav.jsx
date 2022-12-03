abaec81d16aeb4a0cb4d30b0b0d74b476bbafb26
cassnova <daniel.rojascasanova@gmail.com>
2022-12-03 18:33:27 -0300

    added styles and functionality of navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 import Logo from "../../larnu-logo.png";
 
 const Nav = () => {
+  const [clicked, setClicked] = useState(false);
   return (
     <nav className="nav-container">
-      <div className="">Logo</div>
-      <ul>
-        <li>
-          <NavLink to="/">Inicio</NavLink>
-        </li>
-        <li>
-          <NavLink to="/sobremi">Sobre Mi</NavLink>
-        </li>
-        <li>
-          <NavLink to="/portafolio">Portafolio</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contacto">Contacto</NavLink>
-        </li>
-      </ul>
+      <NavLink to="/">
+        <img src={Logo} alt="Logo LarnU" className="logo" />
+      </NavLink>
+      <div>
+        <ul className={clicked ? "active" : null}>
+          <li>
+            <NavLink to="/">Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/sobremi">Sobre Mi</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portafolio">Portafolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacto">Contacto</NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div id="mobile">
+        <i
+          id="bar"
+          className={`fas fa-bars ${clicked ? "fas fa-times" : "fas fa-bars"}`}
+          onClick={() => setClicked(!clicked)}
+        ></i>
+      </div>
     </nav>
   );
 };
