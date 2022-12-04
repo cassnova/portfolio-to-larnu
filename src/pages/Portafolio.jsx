d9576481a1eae8c800d6bf0687452d37773713cc
cassnova <daniel.rojascasanova@gmail.com>
2022-12-03 23:13:24 -0300

    added styles to portafolio page

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./styles/Portafolio.css";
+import Footer from "../components/footer/Footer";
+import Nav from "../components/nav/Nav";
+import Proyectos from "../components/proyectos/Proyectos";
+import Data from "../bd";
 
 const Portafolio = () => {
   return (
-    <div>
-      <h2>Portafolio</h2>
+    <div className="portafolio-container">
+      <Nav />
+      <Proyectos data={Data} />
+      <Footer />
     </div>
   );
 };
